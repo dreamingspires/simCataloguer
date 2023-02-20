6d6347cf915bc0ef0d773eed88578daf40b3adc7
mark-todd <markpeter.todd@hotmail.co.uk>
2023-02-20 13:41:27 +0000

    First working with new interface

--- examples/test_writer.py
+++ examples/test_writer.py
@@ -1,21 +1,8 @@
 from simCataloguer import Writer
-import pixray_module
-from pathlib import Path
+
 writer = Writer()
-writer.train_model('obama_sotu.txt', 'simObama')
-print(writer.generate(run_name='simObama'))
-
-
-path = Path("outputs/hairout")
-
-pixray_module.run(
-    "pandas made of shiny metal",
-    "vdiff",
-    quality="better",
-    custom_loss="edge, symmetry",
-    edge_color="grey",
-    num_cuts=10,
-    outdir=str(path.absolute()),
-    make_video = False,
-    save_intermediates = False
+writer(
+    'obama_sotu.txt',
+    'simObama',
+    'outputs/hairout',
 )
--- simCataloguer/__init__.py
+++ simCataloguer/__init__.py
@@ -2,8 +2,11 @@ import gpt_2_simple as gpt2
 import sys
 from pathlib import Path, PurePath
 import tensorflow as tf
+import pixray_module
 
-def get_rel_path(directory: str, default_dir: Path) -> Path:
+def get_rel_path(directory: str | Path, default_dir: Path) -> Path:
+    if isinstance(directory, Path):
+        return directory
     if PurePath(directory).is_absolute():
         return Path(directory)
     else:
@@ -12,7 +15,7 @@ def get_rel_path(directory: str, default_dir: Path) -> Path:
 class Writer:
     def __init__(
         self, 
-        model_dir: str = 'current_models', 
+        model_dir: str | Path = 'current_models', 
         model_name='124M'
     ) -> None:
         self.default_dir = Path.cwd() / sys.argv[0]
@@ -25,17 +28,22 @@ class Writer:
                 pass
 
         self.sess = None
-        
+        self.model_was_trained: bool = False
 
     def train_model(
             self, 
-            file_name: str, 
+            file_name: str | Path, 
             run_name: str,
-            checkpoint_dir: str = 'checkpoint',  
+            checkpoint_dir: str | Path = 'checkpoint',  
         ):
-        checkpoint_path = get_rel_path(checkpoint_dir, self.default_dir)
-
+        if not isinstance(checkpoint_dir, Path):
+            checkpoint_dir = Path("outputs/hairout")
+            checkpoint_path = get_rel_path(checkpoint_dir, self.default_dir)
+        else:
+            checkpoint_path = checkpoint_dir
+        
         train_path = get_rel_path(file_name, self.default_dir)
+        
         tf.compat.v1.reset_default_graph()
         if self.sess is None:
             self.sess = gpt2.start_tf_sess()
@@ -53,6 +61,7 @@ class Writer:
             checkpoint_dir = str(checkpoint_path),
             model_dir = str(self.model_path)
         )
+        self.model_was_trained = True
 
     def generate(
         self, 
@@ -61,7 +70,7 @@ class Writer:
         temperature: float = 0.8, 
         n_samples: int = 1, 
         batch_size: int = 1
-    ):
+    ) -> list[str]:
         # Choose or refuse one text at a time - 300 to 500 words
 
         if self.sess is None:
@@ -72,6 +81,61 @@ class Writer:
             length=length, 
             temperature=temperature,
             nsamples=n_samples, 
-            batch_size=batch_size
+            batch_size=batch_size,
+            return_as_list = True
         )
+
+
+    def make_pixray(
+        self,
+        prompt: str,
+        output: str | Path,
+        quality: str = "better",
+        num_cuts: int = 10
+    ):
+        if not isinstance(output, Path):
+            output = Path("outputs/hairout")
+        pixray_module.run(
+            prompt[0:70],
+            "vdiff",
+            quality=quality,
+            custom_loss="edge, symmetry",
+            edge_color="grey",
+            num_cuts=num_cuts,
+            outdir=str(output.absolute()),
+            make_video = False,
+            save_intermediates = False
+        )
+
+    def __call__(
+        self,
+        input_file: str | Path, 
+        run_name: str, 
+        output: str | Path,
+        checkpoint_dir: str | Path = 'checkpoint',
+        text_length: int = 500, 
+        text_temperature: float = 0.8, 
+        pixray_quality: str = "better",
+        pixray_num_cuts: int = 10
+    ):
+        if not self.model_was_trained:
+            self.train_model(
+                file_name=input_file, 
+                run_name=run_name, 
+                checkpoint_dir=checkpoint_dir
+            )
+        generated_texts = self.generate(
+            run_name=run_name, length=text_length, temperature=text_temperature
+        )
+        self.make_pixray(generated_texts[0], output, pixray_quality, pixray_num_cuts)
+
+    # def train_model(
+    #     self, 
+    #     file_name: str, 
+    #     run_name: str,
+    #     checkpoint_dir: str = 'checkpoint',
+    #     always_retrain: bool = False
+    # ):
+    #     if always_retrain or not (Path(checkpoint_dir) / run_name).exists():
+    #         self._train_model(file_name, run_name, checkpoint_dir)
 
